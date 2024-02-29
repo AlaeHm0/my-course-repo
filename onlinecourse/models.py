4def11f64c15c0d7f938216d4caaa7672da76ac8
AlaeHm0 <hmimoualae49@gmail.com>
2024-02-29 01:45:28 +0000

    add views submit, show_result and update admin page

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,3 +101,31 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+
+# Question Model
+class Question(models.Model):
+    content = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.IntegerField(default=50)
+    def __str__(self):
+        return "Question : " + self.content
+    # method to calculate if the learner gets the score of he question
+    def is_get_score(self, selected_ids):
+        all_answers = self.course_set.filter(is_correct=True).count()
+        selected_correct = self.course_set.filter(is_correct=True, id__on=selected_ids).count()
+        return all_answers == selected_correct
+    
+# Choice Model
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return "Choice : " + self.content
+    
+# Submission Model    
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
